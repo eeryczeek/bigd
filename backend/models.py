7604d651845c23963cd7f71c90e846191945f215
eeryczeek <eryk.ptaszynski@vp.pl>
2024-06-13 14:29:29 +0200

    update stresstests

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List, Set
+from typing import List
 from uuid import UUID
 from datetime import datetime
 
--- backend/tests/stresstests.py
+++ backend/tests/stresstests.py
@@ -1,69 +1,47 @@
 from locust import HttpUser, task, between, SequentialTaskSet
-import random
-
-"""
- Stress Test 1: The client makes the same request very quickly min (10000 times).
- Stress Test 2: Two or more clients make the possible requests randomly (10000 times).
- Stress Test 3: Immediate occupancy of all seats/reservations by 2 clients.
-    The idea is that we have one pool for reservation and 2 clients want to claim as much as possible.
-    A situation where one client claims all is undesirable. 
- Stress Test 4: constant cancellations and seat occupancy. (For the same seat 10000 times)
- Stress Test 5: Update of 1000 reservations. (For example, change the date for all reservations)
 
 
-"""
-
-
-class CassandraStressUser(HttpUser):
+class UserBehavior(HttpUser):
+    host = "http://localhost:8000"
     wait_time = between(0, 0)
 
     @task
     class StressTest1(SequentialTaskSet):
-        def on_start(self):
-            self.endpoint = "http://localhost:8000/movies"
-            self.data = {}
-
-        @task(10000)
-        def make_same_request_quickly(self):
-            self.client.get(self.endpoint, json=self.data)
+        @task
+        def fetch_movies(self):
+            self.client.get("/movies")
 
     @task
     class StressTest2(SequentialTaskSet):
-        def on_start(self):
-            self.endpoints = ["/endpoint1", "/endpoint2"]
-            self.data = {"key": "value"}
-
-        @task(10000)
-        def make_random_requests(self):
-            endpoint = random.choice(self.endpoints)
-            self.client.post(endpoint, json=self.data)
+        @task
+        def fetch_movie_shows(self):
+            movieTitle = "Inception"
+            self.client.get(f"/shows/{movieTitle}")
 
         @task
-        class StressTest3(SequentialTaskSet):
-            def on_start(self):
-                self.endpoint = "/your/cassandra/reservation_endpoint"
-                self.data = {"key": "value"}
+        def fetch_cinema_room(self):
+            cinemaRoomName = "Room 1"
+            self.client.get(f"/rooms/{cinemaRoomName}")
 
-            @task(2)
-            def occupy_all_seats(self):
-                self.client.post(self.endpoint, json=self.data)
-
+    @task
+    class StressTest3(SequentialTaskSet):
         @task
-        class StressTest4(SequentialTaskSet):
-            def on_start(self):
-                self.endpoint = "/your/cassandra/cancellation_endpoint"
-                self.data = {"key": "value"}
+        def create_reservation(self):
+            movieShow = self.client.get("/shows/Inception").json()[0]
+            self.client.post(f"/reservations/{movieShow.show_id}", json={
+                "show_id": movieShow['show_id'],
+                "seat": {"x": 1, "y": 1, "is_reserved": True},
+                "user_mail": "user@example.com"
+            })
 
-            @task(10000)
-            def constant_cancellations_and_occupancy(self):
-                self.client.post(self.endpoint, json=self.data)
-
+    @task
+    class StressTest4(SequentialTaskSet):
         @task
-        class StressTest5(SequentialTaskSet):
-            def on_start(self):
-                self.endpoint = "/your/cassandra/update_endpoint"
-                self.data = {"key": "value"}
+        def fetch_reservations(self):
+            self.client.get("/reservations/user@example.com")
 
-            @task(1000)
-            def update_reservations(self):
-                self.client.put(self.endpoint, json=self.data)
+    @task
+    class StressTest5(SequentialTaskSet):
+        @task
+        def fetch_reservations_by_user(self):
+            self.client.get("/reservations/user/user@example.com")
